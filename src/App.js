83af7532249eca98558295cf64680a60aafb60c0
Oskari Ahonen <ahonenoskari@gmail.com>
2023-03-20 13:48:05 +0200

    Refactor App.js to use useState
    Tasks and completed tasks were hardcoded. Refactored App.js so that
    tasks and completed tasks now use useState.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
+import { useState } from 'react'
+
 function App() {
+  const [tasks, setTasks] =
+    useState(['Koodaile', 'Kato formulat', 'Tiskaile'])
+  const [done, setDone] = useState(['Päikkärit', 'Juo kahvi'])
+
   return (
     <div>
       <h1>ToDo</h1>
 
       <h2>Tasks</h2>
       <ul>
-        <li>Koodaile</li>
-        <li>Kato formulat</li>
-        <li>Tiskaile</li>
+        {
+          tasks.map(task => 
+            <li key={tasks.indexOf(task)}>
+              {task}
+            </li>
+          )
+        }
       </ul>
 
       <h2>Done</h2>
       <ul>
-        <li>Päikkärit</li>
-        <li>Juo kahvi</li>
+        {
+          done.map(task => 
+            <li key={done.indexOf(task)}>
+              {task}
+            </li>
+          )
+        }
       </ul>
 
     </div>
